f3ddfc1e30d8c8c5f1922da76972ee2389222bb6
Yash Dev Singh <127514543+yashdevsingh@users.noreply.github.com>
2024-08-08 18:06:35 +0530

    Time: 50 ms (48.12%) | Memory: 26.9 MB (31.44%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,8 +1,7 @@
 class Solution {
 private:
     bool isPalindrome(string s) {
-        int left = 0;
-        int right = s.length() - 1;
+        int left = 0, right = s.length() - 1;
         while (left < right) {
             if (s[left] != s[right])
                 return false;
