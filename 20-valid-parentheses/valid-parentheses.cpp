ab98b732a89ee28cb36948d7c76f49cf8b4c7f6b
Yash Dev Singh <127514543+yashdevsingh@users.noreply.github.com>
2024-08-12 15:49:19 +0530

    Time: 3 ms (40.95%) | Memory: 7.7 MB (55.93%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    bool isNumBal(string s){
+        int sum=0;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='(') sum += 1; if(s[i]==')') sum -= 1;
+            if(s[i]=='{') sum += 1; if(s[i]=='}') sum -= 1;
+            if(s[i]=='[') sum += 1; if(s[i]==']') sum -= 1;
+        }
+        return sum==0;
+    }
+    bool isOrderCorrect(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]); 
+            else {
+            if (st.empty()) return false;
+            char top = st.top();
+            if ((s[i] == ')' && top == '(') || (s[i] == '}' && top == '{') || (s[i] == ']' && top == '[')) st.pop();
+            else return false;
+            }
+        }
+        return st.empty();
+    }
+public:
+    bool isValid(string s) {
+        if(!isNumBal(s)) return false;
+        if(!isOrderCorrect(s)) return false;
+        return true;
+    }
+};
