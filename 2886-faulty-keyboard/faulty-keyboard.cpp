f141834d83a4a339dcf0282ff2708a3f2c8cedb2
Yash Dev Singh <127514543+yashdevsingh@users.noreply.github.com>
2024-08-12 22:46:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (79.47%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.cpp
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -3,8 +3,8 @@ public:
     string finalString(string s) {
         string s2;
         for(int i=0; i<s.length(); i++){
-            if(s[i]!='i') s2.push_back(s[i]);
             if(s[i]=='i') reverse(s2.begin(), s2.end());
+            else s2.push_back(s[i]);
         }
         return s2;
     }
