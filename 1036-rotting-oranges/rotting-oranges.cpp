1a1834501cf2c13b140355f953e3f4bc15b0f641
Yash Dev Singh <127514543+yashdevsingh@users.noreply.github.com>
2024-08-14 09:26:11 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (72.96%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -5,7 +5,7 @@ public:
         int m=grid[0].size();
         // {{row, column}, time}
         queue <pair<pair<int , int>, int>> q;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int vis[n][m];
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j]==2){
